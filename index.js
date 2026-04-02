6258fc5f96f073b4c5b66b83438f17db8df93898
AntonGorov195 <antongorov195@gmail.com>
2026-04-02 16:51:45 +0300

    multiple different borders

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-// if (languageSelect !== undefined && languageSelect !== null) {
-//     /**
-//      * 
-//      * @param {"עברית" | "English"} lang 
-//      */
-//     function selectLang(lang) {
-//         switch (lang) {
-//             case "עברית":
-//                 heMain.style.display = "block";
-//                 enMain.style.display = "none";
-//                 break;
-//             case "English":
-//                 heMain.style.display = "none";
-//                 enMain.style.display = "block";
-//                 break;
-//             default:
-//                 alert("unidentified language \"" + lang + "\" please open an issue.");
-//                 break;
-//         }
-//     }
-//     languageSelect.addEventListener("change", (e) => selectLang(e.target.value));
-//     selectLang(languageSelect.value);
-// }
+
+/**
+ * background page
+ * Background body
+ * Background body 2
+ * border color
+ * text color
+ * link color
+ */

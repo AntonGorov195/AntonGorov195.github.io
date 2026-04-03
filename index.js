2730af206404313a19d8bf0d485955da94d08a22
AntonGorov195 <antongorov195@gmail.com>
2026-04-03 13:42:29 +0300

    changed the main page

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 
-/**
- * background page
- * Background body
- * Background body 2
- * border color
- * text color
- * link color
- */
+const panels = [
+    aboutMeContainer,
+    projectsContainer,
+    articlesContainer,
+];
+aboutMeButton.addEventListener("click", () => {
+    openPanel(aboutMeContainer)
+})
+projectsButton.addEventListener("click", () => {
+    openPanel(projectsContainer)
+})
+articlesButton.addEventListener("click", () => {
+    openPanel(articlesContainer)
+})
+function openPanel(panel) {
+    panels.forEach((panel) => panel.style.display = "none");
+    panel.style.display = "block"
+}
